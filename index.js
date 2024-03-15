43197916bdd43230466a78891012e343a7e5b56e
Clayton Nicholson <claynicholson199@gmail.com>
2024-03-15 16:29:39 -0500

    finished part of assignment

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { clear } = require('console');
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
@@ -9,10 +10,48 @@ function ask(questionText) {
 
 start();
 
+
 async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
-  process.exit();
+
+  let min = 1;
+  let max = 100;
+  let guess = Math.floor((min + max) /2);
+  
+  let attempts = 0;
+  let running = true;
+  
+  while (running) {
+    let response = await ask (`Is your number... ${guess}? Type y for yes and n for no\n`)
+    
+    if (response.toLowerCase() === `n`) {
+      console.log("Fiddlesticks!")
+
+    } else if (response.toLowerCase() === `y` ) {
+      console.log(`I guessed it right! The number you chose is ${guess}!`)
+      process.exit()
+
+    } else if (response.toLowerCase !== `n` || `y`) {
+      console.log("Please enter a valid response")
+    }
+    
+    response = await ask(`Is it higher or lower? Use h for higher and l for lower\n`);
+    
+    
+    if (response === `h`) {
+      min = guess;
+    guess = Math.floor((min + max) /2);
+  } else if (response === `l`) {
+    max = guess;
+    guess = Math.floor((min + max) /2);
+  } else {
+    console.log("Please choose a valid response.")
+  }
+  
+
+}
+
+process.exit();
 }
